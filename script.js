7cb8a604f3b61a8bf77b589c9db8111bfb30975e
akin-bh <akin.bhattarai@gmail.com>
2025-01-09 17:31:24 -0700

    first-commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Frontend Developer","Web Designer","Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+
+
+})
