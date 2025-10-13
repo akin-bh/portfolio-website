4626b8e6d308cdd6d682543d51d65ff31cf0358b
Anuj Bhattarai <akinbhattarai@gmail.com>
2025-10-12 19:52:30 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var typed = new Typed(".multiple-text",{
-    strings: ["Frontend Developer","Web Designer","Graphic Designer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop:true
-
-
-})
